119c0d5b15d0925006e728f6f0c00a7a613e1a93
Budi Irawan <deerawan@gmail.com>
2016-11-09 22:38:21 +0700

    fix constant access code section 4.6

--- Section 4/4.6 Class Constants/constant-access.php
+++ Section 4/4.6 Class Constants/constant-access.php
@@ -1,1 +1,13 @@
+<?php
 
+class Register {
+    const MIN_USERNAME_LENGTH = 5;
+}
+
+echo Register::MIN_USERNAME_LENGTH . PHP_EOL;
+
+$className = 'Register';
+echo $className::MIN_USERNAME_LENGTH . PHP_EOL;
+
+$register = new Register();
+echo $register::MIN_USERNAME_LENGTH . PHP_EOL;
